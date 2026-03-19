e00d4bacc60f09a3611f4d5274292425b226a8f1
anudeep <anudeepm006@gmail.com>
2026-03-19 09:32:45 +0530

    Coderabbit fix for heartbeat at socket

--- src/ws/server.js
+++ src/ws/server.js
@@ -19,8 +19,22 @@ export function attachWebSocketServer(server) {
     maxPayload: 1024 * 1024,
   });
   wss.on("connection", (socket) => {
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
+
     sendJson(socket, { type: "Welcome" });
+
     socket.on("error", (error) => console.log(error));
+  });
+
+  const interval = setInterval(() => {
+    wss.clients.foreach((client) => {
+      if (client.isAlive === false) return client.terminate();
+      client.isAlive = false;
+      client.ping();
+    }, 30000);
   });
 
   function broadCastMatchCreated(match) {
